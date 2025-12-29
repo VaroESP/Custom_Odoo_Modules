c99898a36a261263dc9c32309241db215b14eed2
varoESP <varoclaver@gmail.com>
2025-12-29 13:23:55 +0100

    Update custom security rules module

--- addons/custom_security_rules/__manifest__.py
+++ addons/custom_security_rules/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Security Rules Restriction",
     "version": "18.0.0.1",
     "author": "varoESP",
-    "website": "https://github.com/VaroESP",
+    "website": "https://github.com/VaroESP/Custom_Odoo_Modules",
     "category": "Education",
     "summary": "Add rules for modifying a company's contact information",
     "depends": ['base', 'contacts'],
